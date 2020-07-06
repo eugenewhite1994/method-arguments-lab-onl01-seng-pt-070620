86eb0662902e4a81bea8fa38735a6236691b695c
Eugene White <eugenewhite1994@gmail.com>
2020-07-06 22:54:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,4 @@ introduction("Eugene")
 def introduction_with_language(language,name)
   puts  "I am learning to program in #{language}"
 end
-introduction_with_language("Ruby", "Eugene")
+introduction_with_language("Ruby", #{name})
